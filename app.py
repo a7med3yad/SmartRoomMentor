59f2a86b23e35b1bc0f1749fda12d0cc5b2c5be7
Ahmed Ayad <ahmed.ibrahim01974@gmail.com>
2025-08-31 01:38:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,26 +6,22 @@ from backend.services.mqtt_service import start_mqtt
 app = Flask(__name__, static_folder="frontend", static_url_path="")
 CORS(app)
 
-# تسجيل الـ routes
+
 app.register_blueprint(iot_bp)
 start_mqtt()
 
-# route للرئيسية → index.html
 @app.route("/")
 def serve_index():
     return send_from_directory(app.static_folder, "index.html")
 
-# أي ملف جوه frontend (html تانية مثلاً)
 @app.route('/<path:path>')
 def serve_frontend(path):
     return send_from_directory(app.static_folder, path)
 
-# نخلي css يترجع من فولدر css/
 @app.route('/css/<path:filename>')
 def serve_css(filename):
     return send_from_directory("css", filename)
 
-# نخلي js يترجع من فولدر js/
 @app.route('/js/<path:filename>')
 def serve_js(filename):
     return send_from_directory("js", filename)
